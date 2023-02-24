219c7c63d7e3682703b5b7308b399ed85014565a
Esteban297 <estebancristancho9@gmail.com>
2023-02-23 21:36:17 -0500

    Lab 4 Terminado

--- src/main/java/edu/escuelaing/arep/app/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/app/HttpServer.java
@@ -25,15 +25,15 @@ public class HttpServer {
     public void run(String[] args) throws IOException, ClassNotFoundException, InvocationTargetException, IllegalAccessException {
         springApplication spring = new springApplication();
         ArrayList<String> classes = spring.getClassComponent(new ArrayList<>(), ".");
-        for (String className: classes) {
+        for (String className : classes) {
             Class c = Class.forName(className);
             Method[] methods = c.getMethods();
-            for (Method m  : methods){
-                if(m.isAnnotationPresent(RequestMapping.class)){
+            for (Method m : methods) {
+                if (m.isAnnotationPresent(RequestMapping.class)) {
                     try {
                         String URL = m.getAnnotation(RequestMapping.class).value();
-                        services.put(URL,m);
-                    } catch (Throwable x){
+                        services.put(URL, m);
+                    } catch (Throwable x) {
                         x.printStackTrace();
                     }
                 }
@@ -66,36 +66,35 @@ public class HttpServer {
             boolean first_line = true;
             outputStream = clientSocket.getOutputStream();
             String request = "/simple";
-            String verb = "";
             while ((inputLine = in.readLine()) != null) {
                 if (first_line) {
                     request = inputLine.split(" ")[1];
                     first_line = false;
-                    verb = inputLine.split(" ")[0];
                 }
                 if (!in.ready()) {
                     break;
                 }
             }
-            if (Objects.equals(verb, "GET")) {
-                if(services.containsKey(request)) {
+            if (!Objects.equals(request, "/")) {
+                if (services.containsKey(request)) {
                     outputLine = services.get(request).invoke(null).toString();
                 }
-            }else {
-            outputLine = "HTTP/1.1 200 OK\r\n" +
-                    "Content-type: text/html\r\n" +
-                    "\r\n" +
-                    "<!DOCTYPE html>"
-                    + "<html>"
-                    + "<head>"
-                    + "<meta charset=\"UTF-8\">"
-                    + "<title>404</title>\n"
-                    + "</head>"
-                    + "<body>"
-                    + "Use metodos GET"
-                    + "</body>"
-                    + "</html>";
-        }
+            } else {
+                outputLine = "HTTP/1.1 200 OK\r\n" +
+                        "Content-type: text/html\r\n" +
+                        "\r\n" +
+                        "<!DOCTYPE html>"
+                        + "<html>"
+                        + "<head>"
+                        + "<meta charset=\"UTF-8\">"
+                        + "<title>404</title>\n"
+                        + "</head>"
+                        + "<body>"
+                        + "Pagina inicial\r\n"
+                        + "Esteban"
+                        + "</body>"
+                        + "</html>";
+            }
             out.println(outputLine);
             out.close();
             in.close();
@@ -103,45 +102,6 @@ public class HttpServer {
         }
         serverSocket.close();
     }
-
-
-
-    private static String htmlOriginal() {
-        return "HTTP/1.1 200 OK\r\n"
-                + "Content-Type: text/html\r\n"
-                + "\r\n"
-                + "<!DOCTYPE html>\n" +
-                "<html>\n" +
-                "    <head>\n" +
-                "        <title>Form Example</title>\n" +
-                "        <meta charset=\"UTF-8\">\n" +
-                "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-                "    </head>\n" +
-                "    <body>\n" +
-                "        <h1>GET</h1>\n" +
-                "        <form action=\"/hello\">\n" +
-                "            <label for=\"name\">Title:</label><br>\n" +
-                "            <input type=\"text\" id=\"name\" name=\"name\" value=\"John\"><br><br>\n" +
-                "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsg()\">\n" +
-                "        </form> \n" + "<br>" +
-                "        <div id=\"getrespmsg\"></div>\n" +
-                "\n" +
-                "        <script>\n" +
-                "            function loadGetMsg() {\n" +
-                "                let nameVar = document.getElementById(\"name\").value;\n" +
-                "                const xhttp = new XMLHttpRequest();\n" +
-                "                xhttp.onload = function() {\n" +
-                "                    document.getElementById(\"getrespmsg\").innerHTML =\n" +
-                "                    this.responseText;\n" +
-                "                }\n" +
-                "                xhttp.open(\"GET\", \"/title?name=\"+nameVar);\n" +
-                "                xhttp.send();\n" +
-                "            }\n" +
-                "        </script>\n" +
-                "\n" +
-                "</html>";
-    }
-
 
     public OutputStream getOutputStream() {
         return outputStream;
